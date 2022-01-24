18e246a89953fe211c1cb44cf66bafed24415ee9
Laguda Stefano <lagudadev@gmail.com>
2022-01-24 08:38:58 +0100

    feat: install react-icons and added delete button icon

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 
 const List = ({ data }) => {
   // console.log(data);
@@ -23,7 +24,9 @@ const Person = ({ id, nome, stato, img }) => {
       <div className="person-info">
         <div className="person-action">
           <h4>{nome}</h4>
-          <button className="btn">Remove</button>
+          <button className="btn">
+            <RiDeleteBack2Fill className="icon" />
+          </button>
         </div>
         <p>{stato}</p>
       </div>
