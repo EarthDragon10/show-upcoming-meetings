daa7cb8daf5c056919d720f33d773c07fd9cfd27
Laguda Stefano <lagudadev@gmail.com>
2022-01-24 09:06:08 +0100

    feat: added logic of reload/remove items

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import data from "./data";
 
 function App() {
   const [people, setPeople] = useState(data);
-  // console.log(people);
+
+  const reloadAllItem = () => {
+    setPeople(data);
+  };
+
+  const removeItem = (id) => {
+    setPeople((prevValue) => {
+      return prevValue.filter((value) => value.id !== id);
+    });
+  };
 
   return (
     <section>
       <div className="container">
         <h2 style={{ color: "var(--bg-blue)" }}>Prossimi incontri</h2>
         <div className="people-list">
-          <List data={people} />
+          <List data={people} removeItem={removeItem} />
         </div>
         <div className="btn-group">
-          <button className="btn btn-reset">Reload</button>
-          <button className="btn btn-delete">Delete All</button>
+          <button className="btn btn-reset" onClick={reloadAllItem}>
+            Reload
+          </button>
+          <button className="btn btn-delete" onClick={() => setPeople([])}>
+            Delete All
+          </button>
         </div>
       </div>
     </section>
--- src/List.js
+++ src/List.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { RiDeleteBack2Fill } from "react-icons/ri";
 
-const List = ({ data }) => {
+const List = ({ data, removeItem }) => {
   // console.log(data);
+
   return (
     <ul className="user-list">
       {data.map((el) => {
         return (
           <li key={el.id}>
-            <Person {...el} />
+            <Person {...el} removeItem={removeItem} />
           </li>
         );
       })}
@@ -16,15 +17,16 @@ const List = ({ data }) => {
   );
 };
 
-const Person = ({ id, nome, stato, img }) => {
+const Person = ({ id, nome, stato, img, removeItem }) => {
   // console.log({ PersonComponent: props });
+
   return (
     <article key={id}>
       <img src={img} alt="person-img" className="person-img"></img>
       <div className="person-info">
         <div className="person-action">
           <h4>{nome}</h4>
-          <button className="btn">
+          <button className="btn" onClick={() => removeItem(id)}>
             <RiDeleteBack2Fill className="icon" />
           </button>
         </div>
